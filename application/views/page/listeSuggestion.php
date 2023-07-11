fa0cf85d9a75ae370125a3de8bc333cbf954aaad
idealy-ma <91415549+Tokyram@users.noreply.github.com>
2023-07-11 09:32:12 +0200

    update suggestion V5

---
+++ application/views/page/listeSuggestion.php
@@ -0,0 +1,101 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/profil.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+
+    <title>Profil de personne</title>
+    <style>
+        .hidden {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+
+<div class="content">
+    <div id="modal" class="modal" style="width: 800px; ">
+      <div class="modals-content">
+
+        <h2>SUGGESTION</h2>
+
+        <div class="card">
+          <div class="header">
+              <span class="title">Suggestion 1</span>
+              <!-- <span class="price">Free</span> -->
+          </div>
+          <!-- <p class="desc">Etiam ac convallis enim, eget euismod dolor.</p> -->
+          <ul class="lists">
+              <li class="list">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                  </svg>
+                  <span>Aenean quis</span>
+              </li>
+              <li class="list">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                  </svg>
+                  <span>Morbi semper</span>
+              </li>
+              <li class="list">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                  </svg>
+                  <span>Tristique enim nec</span>
+              </li>
+              <li class="list">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                  </svg>
+                  <span>Tristique enim nec</span>
+              </li>
+          </ul>
+      </div>
+
+      <div class="card">
+        <div class="header">
+            <span class="title">Suggestion 1</span>
+            <!-- <span class="price">Free</span> -->
+        </div>
+        <!-- <p class="desc">Etiam ac convallis enim, eget euismod dolor.</p> -->
+        <ul class="lists">
+            <li class="list">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                </svg>
+                <span>Aenean quis</span>
+            </li>
+            <li class="list">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                </svg>
+                <span>Morbi semper</span>
+            </li>
+            <li class="list">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                </svg>
+                <span>Tristique enim nec</span>
+            </li>
+            <li class="list">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                </svg>
+                <span>Tristique enim nec</span>
+            </li>
+        </ul>
+    </div>
+          <button id="modal-close-btn" class="modal-close-btn">Fermer</button>
+          <a href="#"><button type="button" class="action" >REGENERER</button></a>
+          <a href="#"><button type="button" class="action" >VALIDER</button></a>
+      </div>
+  </div>
+  </div>   
+
+</body>
+</html>
